420b6e9a96a564f7d05b5dd7f72f239c7cefa0a9
Gerardo233 <miguelmr1107@gmail.com>
2023-08-21 19:20:47 -0600

    Substraction function added and name update The Substraction function inicialization has been done and the its name was updated from Substractions to Substraction.

--- cpp/operacionesBasicas.cpp
+++ cpp/operacionesBasicas.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int Addition(int, int);
-int Substractions(int, int);
+int Substraction(int, int);
 int Multiply(int, int);
 int Divide(int, int);
 int SquareRoot(int, int);
@@ -16,3 +16,16 @@ int Addition(int a, int b)
 {
     return a + b;
 }
+
+int Substraction(int a, int b)
+{
+    if (a < b)
+    {
+        cout << "La resta es negativa";
+        return a - b;
+    }
+    else
+    {
+        return a - b;
+    }
+};
