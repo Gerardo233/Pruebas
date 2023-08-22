71dbebe9342e197f7924aea4df48351700419940
Gerardo233 <miguelmr1107@gmail.com>
2023-08-21 18:40:58 -0600

    Declaration of the operations added

--- cpp/operacionesBasicas.cpp
+++ cpp/operacionesBasicas.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+int Addition(int, int);
+int Substractions(int, int);
+int Multiply(int, int);
+int Divide(int, int);
+int SquareRoot(int, int);
+
 int main()
 {
     return 0;
